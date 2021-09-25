c83682526fdf3ed2c933928526a7e35d402083c1
Jaewan <jamieecode@gmail.com>
2021-09-25 23:29:22 +0900

    redux example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const weight = 100;
+
+function reducer(state = weight, action) {
+  if (action.type === "INCREASE") {
+    state++;
+    return state;
+  } else if (action.type === "DECREASE") {
+    state--;
+    return state;
+  } else {
+    return state;
+  }
+}
+
+let store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
